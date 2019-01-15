a82adb594192a55c86eeb30535cc12ea0b7f04e0
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2019-01-15 08:52:30 -0200

    Orquestrador e Constantes

---
+++ AutomatizadorDeImpressao/app/AutomatizadorDeImpressao.Domain/Orquestrador.cs
@@ -0,0 +1,27 @@
+﻿using AutomatizadorDeImpressao.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatizadorDeImpressao.Domain
+{
+    public class Orquestrador
+    {
+        List<Arquivo> arquivos = new List<Arquivo>();
+
+
+
+    }
+
+    /*public bool VerificarArquivos()
+    {
+        string caminho = ConfigurationManager.AppSettings[Constantes.CAMINHO_DOS_ARQUIVOS];
+        if (File.Exists(caminho))
+            return true;
+        return false;
+    }*/
+}
