eacbf0d112a522185d5c10ce01c523e398489a7b
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2019-01-14 23:05:45 -0200

    Criação das Entities

---
+++ AutomatizadorDeImpressao/app/AutomatizadorDeImpressao.Domain/Entities/Arquivo.cs
@@ -0,0 +1,19 @@
+﻿namespace AutomatizadorDeImpressao.Domain.Entities
+{
+    public class Arquivo
+    {
+        public Arquivo(string nome, string local, string extensão, int copias)
+        {
+            this.nome = nome;
+            this.local = local;
+            this.extensão = extensão;
+            this.copias = copias;
+        }
+
+        public string nome { get; private set; }
+        public string local { get; private set; }
+        public string extensão { get; private set; }
+        public int copias { get; private set; }
+
+    }
+}
---
+++ AutomatizadorDeImpressao/app/AutomatizadorDeImpressao.Domain/Entities/Impressora.cs
@@ -0,0 +1,16 @@
+﻿namespace AutomatizadorDeImpressao.Domain.Entities
+{
+    public class Impressora
+    {
+
+        public Impressora(string nome, Arquivo arquivo)
+        {
+            this.nome = nome;
+            this.quantidadeDeCopias = arquivo.copias;
+        }
+
+        public string nome { get; set; }
+        public int quantidadeDeCopias { get; private set; }
+
+    }
+}
